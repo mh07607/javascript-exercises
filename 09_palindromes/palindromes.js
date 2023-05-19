de3761bf662ec048f316b0a8d48e207400e9104b
mh07607 <mh07607@st.habib.edu.pk>
2023-05-19 16:50:15 +0500

    until exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newstring = "";
+    string = string.toLowerCase();
+    for(let i=0; i<string.length; i++){
+        if(string[i] == ' ' || string[i] == '.' || string[i] == ',' || string[i] == '!'){
+            continue;
+        }
+        newstring = newstring + string[i];
+    }
+    reverseString = newstring.split("").reverse().join("");
+
+    if(newstring == reverseString){
+        return true;
+    }
+    return false;
 
 };
 
