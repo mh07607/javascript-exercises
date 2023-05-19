2993f8dee6205cbe51cd037f9f3da6d497341e57
mh07607 <mh07607@st.habib.edu.pk>
2023-05-19 14:35:38 +0500

    4 exercises done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    resultant_string = '';
+    for(let i = string.length-1; i >= 0; i--){
+        resultant_string = resultant_string + string[i];
+    }
+    return resultant_string;
 
+    
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    let new_array = [];
+    let is_in_elements = false;
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < elements.length; j++){
+            if(array[i] === elements[j]){
+                is_in_elements = true;
+                break;
+            }
+        }
+        if(is_in_elements === false){
+            new_array.push(array[i]);
+        }
+        is_in_elements = false;
+    }
+    console.log(new_array);
+    return new_array;
 };
 
 // Do not edit below this line
